3f0f7a78605f416ce63a0e61f1ee8b5752714ef9
leodavinci360 <aremu@susqu.edu>
2015-02-26 18:51:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/leodavinci360/criminalintent/Crime.java
@@ -0,0 +1,50 @@
+package com.example.leodavinci360.criminalintent;
+
+import java.util.UUID;
+import java.util.Date;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public void setTitle(String title) {
+
+        mTitle = title;
+    }
+
+    public UUID getId() {
+
+        return mId;
+    }
+
+    public String getTitle() {
+
+        return mTitle;
+    }
+
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+}
